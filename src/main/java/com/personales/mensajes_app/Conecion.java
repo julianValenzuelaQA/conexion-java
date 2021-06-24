fde80eadc987c452b5f495fa4cba5e80d07d615d
katazztros <jovalenzuela.53@gmail.com>
2021-06-24 13:38:19 -0500

    MI PRIMER COMMIT

---
+++ src/main/java/com/personales/mensajes_app/Conecion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.personales.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author joval
+ */
+public class Conecion {
+    public Connection get_connection(){
+    Connection conection = null;
+    try{
+        conection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
+        if(conection != null){
+            System.out.println("conexion exitosa");
+        }
+    }catch(SQLException e){
+        System.out.println(e);
+    }
+    return conection;
+    }
+    
+}
---
+++ src/main/java/com/personales/mensajes_app/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.personales.mensajes_app;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author joval
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Conecion conexion = new Conecion();
+        
+        try(Connection cnx = conexion.get_connection() ){
+        
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+}
